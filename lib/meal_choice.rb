621339040bbe0509a8f2d5c3465a5ba5b2b1dde2
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-05 05:01:41 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,7 @@
-def meal_choice(protein = "", veg1, veg2 )
-    puts "A plate of  #{protein} with #{veg1} and #{veg2}."
+def meal_choice(veg1, veg2, protein = "meat")
+  puts 'What a nutritious meal!'
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
+  
   
 end
