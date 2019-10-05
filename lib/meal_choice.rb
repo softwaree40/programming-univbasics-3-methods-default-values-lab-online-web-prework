4182ca3c331417842a0727a25def0624695428c7
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-05 04:47:04 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,6 @@
-def meal_choice(protein = "", veg1, veg2 )
-    puts "A plate of  #{protein} with #{veg1} and #{veg2}."
+def meal_choice(protein = "meat", veg1, veg2 )
+  
+  return "A plate of  #{protein} with #{veg1} and #{veg2}."
+  
   
 end
